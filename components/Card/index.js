8d55f587744e29734740dd95ca696f9a63e36801
kionale <kiona.peng@gmail.com>
2023-01-19 15:44:28 -0800

    files uploaded

---
+++ components/Card/index.js
@@ -0,0 +1,17 @@
+export default function Card({
+    degree = "none",
+    colour = "purple",
+    font = "0",
+    image = "icon/personTwo.png"
+}) {
+    return (
+        <div style={{ backgroundColor: colour, fontSize: font }}>{
+            colour === "red" ?  <img src={'icon/person.png'} width='50' />:
+            colour === "blue" ? <img src={'icon/personThree.png'} width='50' /> :
+            <img src ={image} width ='50' />        
+        }
+            {/* if color == rd do img. : meaning else if is blue, imgTHree, otherwise defalut */}
+        {degree}
+        </div>
+    )
+}
